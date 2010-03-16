448e91d4dd30f752604d404175180b9954703f1e
Albert Sikkema <a.sikkema80@gmail.com>
2010-03-16 20:20:23 +0100

    created my first test

---
+++ src/test/java/com/xebia/adoptimizer/analytics/AnalyticsIntegrationTest.java
@@ -0,0 +1,21 @@
+package com.xebia.adoptimizer.analytics;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: albertsikkema
+ * Date: Mar 16, 2010
+ * Time: 8:12:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AnalyticsIntegrationTest {
+
+    @Test
+    public void shouldInvokeAnalyticsAndGetSomething() {
+        assertThat(true, is(true));
+    }
+}
